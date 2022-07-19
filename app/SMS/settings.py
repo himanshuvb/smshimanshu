1614ede885ac45c1ea43298b624247acf313193e
himanshuvb <savvysmith001@gmail.com>
2022-07-19 18:57:34 +0530

    whitenoiseqq

--- app/SMS/settings.py
+++ app/SMS/settings.py
@@ -127,7 +127,9 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS  = [BASE_DIR/'static',]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
